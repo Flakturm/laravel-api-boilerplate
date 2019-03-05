384be8ec34b58ed7b50065fe866379b4e8399592
ANdy Wu <admin@andy-web-dev.com>
2019-03-06 01:02:07 +0800

    Update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id');
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->primary('id');
+
             $table->rememberToken();
             $table->timestamps();
         });
